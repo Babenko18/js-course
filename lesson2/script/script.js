15dff2cf4e42a649769b0f63eeade7423aa22045
Kostya <kostya.babenko.2999@gmail.com>
2020-03-31 18:12:59 +0300

    сделал урок 2

---
+++ lesson2/script/script.js
@@ -0,0 +1,21 @@
+let money = 1000, 
+income = 'freelans',
+addExpenses = 'internet, taxi',
+deposit = true,
+mission = 1500,
+period = 4;
+
+ console.log(typeof money);
+ console.log(typeof 'income');
+ console.log(typeof deposit);
+
+console.log('addExpenses' .length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " долларов");
+
+console.log( 'addExpenses'.toLowerCase() );
+
+
+let budgetDay = money / 30;
+
+console.log('budgetDay: ', Math.floor (budgetDay));
